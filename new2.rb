7da65ee1ef043faaa9afb92a45e7899d484aa828
shotaro yamamoto <shotoron@gmail.com>
2020-07-15 14:31:45 +0900

    添削お願いいたします

---
+++ new2.rb
@@ -0,0 +1,15 @@
+require 'cgi'
+cgi = CGI.new
+# データを受け取った後、HTMLの形式でレスポンス（反応）を返す
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+  # 受け取ったデータcgi['input']で取り出し、ローカル変数に代入する
+  # 目印の'input'から情報を取り出す
+  get = cgi['new2']
+  # HTMLでレスポンスを返却する
+  "<html>
+    <body>
+      <p>品質が悪いゴーヤの情報</p>
+      <p>#{get}</p>
+    </body>
+  </html>"
+}
